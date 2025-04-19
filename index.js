3e0b675e4e9855ffb4d903f6b8a67870a305b6d0
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-04-19 11:45:06 +0100

    Update code to match Scrimba
    I'll follow Scrimba styling until personal projects

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
         const postsArr = data.slice(0, 5);
-
-        postsArr.forEach((post) => {
-            const postTitle = document.createElement("h3");
-            const postBody = document.createElement("p");
-
-            postTitle.textContent = post.title;
-            postBody.textContent = post.body;
-
-            document.getElementById("blog-posts").appendChild(postTitle);
-            document.getElementById("blog-posts").appendChild(postBody);
-        });
+        let html = "";
+        for (let post of postsArr) {
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `;
+        };
+        document.getElementById("blog-list").innerHTML = html;
     });
